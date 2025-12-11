abd846c45b78599921f85e095251284734193c8a
default <bianca.bertinato@stu.ocsb.ca>
2025-12-11 00:53:17 +0000

    added simple calculator

--- main.ts
+++ main.ts
@@ -17,51 +17,51 @@ console.log("f. round");
 console.log("g. exponent");
 console.log("h. square root");
 
-let choice = prompt("What operation do you want to choose: ");
+const choice = prompt("What operation do you want to choose: ");
 
 if (choice == "a") {
-  let x = Number(prompt("Enter first number: "));
-  let y = Number(prompt("Enter second number: "));
+  const x = Number(prompt("Enter first number: "));
+  const y = Number(prompt("Enter second number: "));
   console.log(x + " + " + y + " = " + (x + y));
 }
 
 else if (choice == "b") {
-  let x = Number(prompt("Enter first number: "));
-  let y = Number(prompt("Enter second number: "));
+  const x = Number(prompt("Enter first number: "));
+  const y = Number(prompt("Enter second number: "));
   console.log(x + " - " + y + " = " + (x - y));
 }
 
 else if (choice == "c") {
-  let x = Number(prompt("Enter first number: "));
-  let y = Number(prompt("Enter second number: "));
+  const x = Number(prompt("Enter first number: "));
+  const y = Number(prompt("Enter second number: "));
   console.log(x + " * " + y + " = " + (x * y));
 }
 
 else if (choice == "d") {
-  let x = Number(prompt("Enter numerator: "));
-  let y = Number(prompt("Enter denominator: "));
+  const x = Number(prompt("Enter numerator: "));
+  const y = Number(prompt("Enter denominator: "));
   console.log(x + " / " + y + " = " + (x / y));
 }
 
 else if (choice == "e") {
-  let x = Number(prompt("Enter number: "));
+  const x = Number(prompt("Enter number: "));
   console.log("Absolute value of " + x + " is " + Math.abs(x));
 }
 
 else if (choice == "f") {
-  let x = Number(prompt("Enter number: "));
+  const x = Number(prompt("Enter number: "));
   console.log("Rounded value is " + Math.round(x));
 }
 
 else if (choice == "g") {
-  let a = Number(prompt("Enter base: "));
-  let b = Number(prompt("Enter exponent: "));
+  const a = Number(prompt("Enter base: "));
+  const b = Number(prompt("Enter exponent: "));
   console.log(a + " raised to " + b + " = " + Math.pow(a, b));
 }
 
 else if (choice == "h") {
   console.log("In order to calculate the square root, you will need a non-negative value:");
-  let x = Number(prompt("Enter value: "));
+  const x = Number(prompt("Enter value: "));
   console.log("The square root of " + x + " is " + Math.sqrt(x));
 }
 
